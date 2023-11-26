fa2845789ef8adac7e59a2ba900af85ccb6f31af
Vu Hoai Nam <namhv2026@gmail.com>
2023-11-26 18:01:31 +0700

    update

--- danh-sach-do-uong.php
+++ danh-sach-do-uong.php
@@ -1,1 +1,26 @@
 <?php
+require "connectDB.php";
+header("Content-Type: application/json");
+// Lấy dữ liệu gửi từ yêu cầu POST
+
+    // Thêm nhân viên mới vào cơ sở dữ liệu
+    $sql = "SELECT * FROM tbl_douong WHERE trangThai= true";
+
+    $list = array();
+//kiểm tra
+if ($result = mysqli_query($conn, $sql)) {
+    while ($row = mysqli_fetch_array($result)) {
+        array_push($list,array(
+            "maDoUong" => $row['maDoUong'],
+            "tenDoUong" => $row['tenDoUong'],
+            "gia" => $row['gia'],
+            "image" => $row['image'],));
+    }
+    echo json_encode($list);
+} else
+    //Hiện thông báo khi không thành công
+    echo 'Không thành công. Lỗi' . mysqli_error($conn);
+
+// Đóng kết nối
+mysqli_close($conn);
+?>
--- login.php
+++ login.php
@@ -3,22 +3,20 @@ require "connectDB.php";
 header("Content-Type: application/json");
 // Lấy dữ liệu gửi từ yêu cầu POST
 $data = json_decode(file_get_contents("php://input"));
-
 if (isset($data->username) && isset($data->password)) {
     $username = $data->username;
     $password = $data->password;
-
     // Kiểm tra thông tin đăng nhập
     $sql = "SELECT * FROM tbl_user WHERE username = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
-        echo json_encode(array("success" => true));
+        echo json_encode(true);
     }else{
-        echo json_encode(array("success" => false));
+        echo json_encode(false);
     }
 } else {
-    echo json_encode(array("message" => "Dữ liệu không hợp lệ"));
+    echo json_encode(false);
 }
 
 // Đóng kết nối
